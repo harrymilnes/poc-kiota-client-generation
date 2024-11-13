ce09ff379d4d749108a5477aba181305e0260dae
Harry Milnes <harry@milnes.org>
2024-11-13 21:32:24 +0000

    Initial commit
    Create readme with required dotnet tools
    
    Update readme
    
    Code removal

---
+++ Example.Integrator/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Kiota.Abstractions.Authentication;
+using Microsoft.Kiota.Http.HttpClientLibrary;
+
+var requestAdapter = new HttpClientRequestAdapter(new AnonymousAuthenticationProvider())
+{
+    BaseUrl = "https://localhost:7218"
+};
+
+var client = new ApiClient.ApiClient(requestAdapter);
+
+Console.WriteLine($"Getting all items from {requestAdapter.BaseUrl}");
+var items = await client.Items.GetAsync();
+if(items == null)
+    throw new Exception("Failed to get items from API");
+
+foreach (var item in items)
+{
+    Console.WriteLine($"Name: {item.Name} Price: {item.Price}");
+}
+
+Console.WriteLine($"Getting tea item from {requestAdapter.BaseUrl}");
+var teaItem = await client.Item.GetAsync(requestConfiguration => requestConfiguration.QueryParameters.Name = "Tea");
+if(teaItem == null)
+    throw new Exception("Failed to get tea item from API");
+
+Console.WriteLine($"Name: {teaItem.Name} Price: {teaItem.Price}");
---
+++ README.md
@@ -0,0 +1,5 @@
+# Required Dotnet Tools
+**Install these otherwise nout's gonna happen.**
+
+ - dotnet tool install --global Microsoft.OpenApi.Kiota --version 1.20.0
+ - dotnet tool install --global Swashbuckle.AspNetCore.Cli --version 7.0.0
